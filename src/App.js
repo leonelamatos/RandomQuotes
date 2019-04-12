c9c8b91d9eace557aa9a5cdd1c57126998426650
Leonel Matos <LMatos@SHNC.JFShea.com>
2019-04-12 15:38:13 -0400

    functions added new file:   .vscode/settings.json modified:   package-lock.json modified:   package.json modified:   src/App.js modified:   src/box.css new file:   src/newtext.js modified:   src/quoteBox.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import QuoteBox from './quoteBox'
-import './App.css';
-
+import './App.css'
 
 class App extends Component {
   render() {
--- src/quoteBox.js
+++ src/quoteBox.js
@@ -1,72 +1,123 @@
 import React from 'react'
 import axios from 'axios'
 import parser from 'html-react-parser'
-
 import './box.css'
 
-class QuoteBox extends React.Component {
+let colors, quoteText, quoteAuthor, t_icon, f_icon, btn;
 
-  constructor(props) {
-    super(props);
+colors = ['#293C56', '#71a0a5', '#6F5E76', '#88A096', '#71a0a5', '#444444', '#567568', '#C9B1BD', '#305f72', '#30BCED', '#73FBD3', '#6b8c42'];
+
+// color = colors[Math.floor(Math.random() * colors.length)];
+function sleeper(ms){
+  return function(x){
+    return new Promise(resolve => setTimeout(() => resolve(x), ms))
+  }
+}
+
+function textOpacity(val){
+  return (
+    quoteText.style.opacity = val,
+    quoteAuthor.style.opacity = val,
+    t_icon.style.opacity = val,
+    f_icon.style.opacity = val
+  )
+}
+
+function changeIconColor(icon,color){
+  icon = document.getElementsByClassName(icon);
+  console.log(icon)
+  for(let i = 0; i < icon.length; i++){
+   icon[i].style.color = color
+  }
+}
+
+function randomColor(color){
+  quoteText = document.querySelector('.text');
+  quoteAuthor = document.querySelector('.author');
+  t_icon = document.querySelector('.t-icon');
+  f_icon = document.querySelector('.f-icon');
+  btn = document.querySelector('.btn')
+  color = color[Math.floor(Math.random() * colors.length)]
+  return (
+    // document.body.style.backgroundColor = color,
+    quoteText.style.color = color,
+    quoteAuthor.style.color = color,
+    btn.style.backgroundColor = color,
+    changeIconColor('fab',color)
+  )
+};
+
+class QuoteBox extends React.Component {
+  constructor(){
+    super();
     this.state = {
-      quotes: '',
-      author:'',
-      Quotes: []
+    quotes: '',
+    author:'',
+    isLoading: false
     }
- 
     this.handleRequest = this.handleRequest.bind(this)
   }
 
   componentDidMount(){
-    let self = this;
-  axios.get(`https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&callback="`)
-    .then(function(response){
-      
-      self.setState({
-        quotes: response.data[0].content,
-        author: response.data[0].title,
+    return axios.get(`https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&timestap=${new Date().getTime()}`)
+    .then(
+      res => {
+      const {content, title} = res.data[0]
+      randomColor(colors)
+
+      // console.log(res.data[0])
+      this.setState({ 
+        quotes: content,
+        author: title,
+        isLoading: false
       })
-    })
+    });
   }
 
-
   handleRequest(){
-    
-  let self = this;
-  axios.get(`https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&callback="`)
-    .then(function(response){
-      alert(response.data[0].content)
-      self.setState({
-        quotes: response.data[0].content,
-        author: response.data[0].title
+
+    textOpacity(0);
+
+    axios.get(`https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&timestampt="${new Date().getTime()}"`)
+    .then(sleeper(500))
+    .then(res => {
+      const {content, title} = res.data[0];
+      textOpacity(1)
+      randomColor(colors)
+
+      this.setState({ 
+        quotes: content,
+        author: title,
+        isLoading: false
       })
-    })
+    });
   }
 
   render() {
     return (
       <div>
+        <div className="box-wrapper">
         <div className='box'>
+          <div className="quote-symbol"></div>
+          <div className="quote-content">
+            <h1>Quote</h1>
             <div className="text">
-              {parser(this.state.quotes)}
-          </div>
-          <p className="author">- {this.state.author.toUpperCase()}</p>
-          <footer>
-            <div className="mediaIcons">
-              <i className="fab fa-twitter-square"></i>
-              <i className="fab fa-facebook-square"></i>
+                {parser(this.state.quotes)}
+              </div>
+              <p className="author">- {parser(this.state.author.toUpperCase())}</p>
+              <footer>
+                <button className="btn btnQuote" onClick={this.handleRequest}>New Quote</button>
+                <div className="mediaIcons">
+                  <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/"><i className="fab fa-twitter-square t-icon"></i></a>
+                  <a href="http://facebook.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square f-icon"></i></a>
+                </div>
+              </footer>
             </div>
-          </footer>
-        </div>
-        <button className="btn btnQuote" onClick={this.handleRequest}>New Quote</button>
-
+          </div>
+          </div>
       </div>
-
-
-
     )
   }
-
 }
 
 export default QuoteBox;
