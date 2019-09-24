4243a9a49e4b3a59b79be116908b8d5830fef5c4
leonelamatos <leonelamatos@gmail.com>
2019-09-24 07:57:58 -0400

    Updates

---
+++ precache-manifest.75f2b288f3bc510a44c762d51966957c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8da31e79566d4c7b174",
+    "url": "/RandomQuotes/static/js/runtime~main.7df316e3.js"
+  },
+  {
+    "revision": "e9c4d3ae787e4aada3f6",
+    "url": "/RandomQuotes/static/js/main.97623a19.chunk.js"
+  },
+  {
+    "revision": "d64ec221d3cbd8fbb73c",
+    "url": "/RandomQuotes/static/js/2.722dd2a5.chunk.js"
+  },
+  {
+    "revision": "e9c4d3ae787e4aada3f6",
+    "url": "/RandomQuotes/static/css/main.fd3e635c.chunk.css"
+  },
+  {
+    "revision": "e6729e429ecfdce10a4c267db639ecf5",
+    "url": "/RandomQuotes/index.html"
+  }
+];
